455c7cbf79ff5e325097d3c8352e2dee1377619b
MarufaKompass <Marufa@hellokompass.com>
2024-10-20 12:12:03 +0600

    check hero section

--- components/heroSection/HeroBottom.tsx
+++ components/heroSection/HeroBottom.tsx
@@ -3,7 +3,7 @@ import Seperator from "@/components/seperator";
 const HeroBottom = () => {
     return (
 <div>
-<div className=' bg-[#130F0C] bg-opacity-95  py-6 z-50'>
+<div className=' bg-[#130F0C] bg-opacity-95  py-[25px] z-50'>
         <div className='container  flex justify-between'>
           <div className='flex items-center'>
             <span className=" xl:px-6  px-0 font-cinzel 2xl:text-[20px]  xl:text-[16px]   leading-[24px] font-medium bg-gradient-to-b from-[#FFF1DE] to-[#8C6742] text-transparent bg-clip-text  ">BUILD & CONQUER</span>
