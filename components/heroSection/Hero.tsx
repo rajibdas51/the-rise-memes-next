165be6acb452b5f12602bfaa89e0e93ebcd78b28
MarufaKompass <Marufa@hellokompass.com>
2024-10-29 17:18:11 +0600

    font issue

--- components/heroSection/Hero.tsx
+++ components/heroSection/Hero.tsx
@@ -1,9 +1,6 @@
 "use client";
 import React from "react";
-// import Image from "next/image";
-// import FirstHead from "@/assets/images/first-head.svg";
-// import SecondHead from "@/assets/images/second-head.svg";
-// import ThirdHead from "@/assets/images/last-head.svg";
+
 import heroBg from "../../assets/images/hero-bg.png";
 import HeroForm from "./HeroForm";
 
